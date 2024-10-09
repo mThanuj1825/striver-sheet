c4d669437d19fee6e550130cfa41e80b6b8b4daa
M Thanuj <thanujmullaguri1825@gmail.com>
2024-10-09 19:05:29 +0530

    Version 2

--- frontend/src/components/Question.jsx
+++ frontend/src/components/Question.jsx
@@ -3,9 +3,9 @@ import { Button } from "@/components/ui/button.jsx";
 export function Question({ question, handleMarkCompleted, dashboard }) {
 	return (
 		<li className="pb-10">
-			<div className="flex flex-row sm:flex-row justify-between items-start sm:items-center p-4">
+			<div className="flex flex-col sm:flex-row justify-between items-start sm:items-center p-4">
 				<h4 className="scroll-m-20 sm:text-base md:text-xl font-semibold tracking-tight mr-2">
-					{ question.number }
+					Q{ question.number }:
 				</h4>
 				<h4 className="scroll-m-20 sm:text-base md:text-xl font-semibold tracking-tight mr-2">
 					{ question.title }
